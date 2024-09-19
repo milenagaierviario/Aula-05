41a6cc19bda39a4cc2e67273ce7ea01f1506a712
Milena <mgaier234@gmail.com>
2024-09-19 11:50:42 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+
+//A cor favorita de FULANO é COR
+
+/*let COR = prompt("Qual é a sua cor favorita?")
+let FULANO = prompt("Qual seu nome?")
+const frase = "A cor favorita de" + FULANO + "é " + COR + */
+
+/*const frase = "Hoje é dia dezenove e está quente"
+const maiúscula = frase.toUpperCase()
+
+console.log(maiúscula)
+
+const frase01 = "Hoje é dia dezenove e está quente"
+const fraseNova = frase.replaceAll( "o" , "i" )
+
+console.log(fraseNova)
+
+const frase02 = "Hoje é dia dezenove e está quente"
+
+console.log(frase02.length)*/
+
+
+const raçasCachorros =   [ "BorderCollie" , "GoldenRetriver" , "Corgi" , "Beagle" , "Galgo" ]
+
+const raça = prompt( "Insira um número de 0 a 4" )
+
+const raçaEscolhida = raçasCachorros[0]
+
+console.log(raçaEscolhida)
+
+const valores = [ 1 , 2 , 3 , 4 , 5 , 6 ]
+
+console.log(valores.length)
+
+valores.push(7)
+console.log(valores)
+valores.splice(3, 2) 
+
+console.log(valores.length)
